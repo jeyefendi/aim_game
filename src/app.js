f57eb250ce17b7dbadbbc39226ed47aedd8f691f
jeyefendi <jeyhunfarajov@gmail.com>
2022-10-23 16:27:21 +0300

    breakpoint - function startGame

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+
+let time = 0
+
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'))
+        screens[1].classList.add('up')
+        startGame()
+    }
+}) 
+
+function startGame() {
+    timeEl.innerHTML = `00:${time}`
+}
